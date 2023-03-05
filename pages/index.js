6bace9c59c6e0499ffa6b99daaa3a24b1674b5b4
Khalid759 <khalid.ali@kraftshala.com>
2023-03-05 23:44:59 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Script from "next/script";
 import profile from "./profile-image.png";
 
 const validateEmail = (email) => {
